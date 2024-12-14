52c043f0bdc681ca945f22fb37ac4dff94b3961d
Jasiek <u119_jangaw_waw@technischools.com>
2024-12-14 18:35:57 +0100

    single_user_get

--- app/app.py
+++ app/app.py
@@ -29,5 +29,18 @@ def users_get():
     return jsonify(reading_users), 200
 
 
+@app.route("/users/<int:id>")
+def single_user_get(id):
+    users = reading_users()
+
+    for user in users:
+        if user["id"] == id:
+            wanted = user
+            break
+    if wanted:
+        return jsonify(wanted), 200
+    return jsonify({"error": "User not found"}), 400
+
+
 if __name__ == "__main__":
     app.run()
